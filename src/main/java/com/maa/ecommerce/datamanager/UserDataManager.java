32bf546674ff3c596d29bc835435f1c1e3b3de40
Bhavik Savaliya <savaliyabhavic@gmail.com>
2018-09-07 22:53:28 +0530

    Fixed bug with dbcp2 pooled connection

--- src/main/java/com/maa/ecommerce/datamanager/UserDataManager.java
+++ src/main/java/com/maa/ecommerce/datamanager/UserDataManager.java
@@ -5,44 +5,51 @@ import com.maa.ecommerce.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementCreator;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.core.RowMapperResultSetExtractor;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Service;
 
-import java.security.SecureRandom;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.List;
 import java.util.Map;
 
 @Service
-public class UserDataManager {
+public class UserDataManager
+{
 
     @Autowired
     JdbcTemplate jdbcTemplate;
 
-    public User insertUser(User user) {
+    public User insertUser(User user)
+    {
 
         //TODo update code in a way that end point recieves proper id in the value
         String query = "Insert into user values(NULL, ?, ?, ?, ?, ?)";
 
-        try{
+        try
+        {
 
-            KeyHolder key = new GeneratedKeyHolder();
-            this.jdbcTemplate.update(new PreparedStatementCreator()
+            try (Connection conn = this.jdbcTemplate.getDataSource().getConnection())
             {
-                @Override
-                public PreparedStatement createPreparedStatement(Connection connection) throws SQLException
+                try (PreparedStatement statement = conn.prepareStatement(query, Statement.RETURN_GENERATED_KEYS))
                 {
+                    statement.setString(1, user.getName());
+                    statement.setString(2, user.getEmail());
+                    statement.setString(3, user.getUsername());
+                    statement.setString(4, user.getPassword());
+                    statement.setInt(5, user.getRoleid());
 
+                    statement.executeUpdate();
+                    try (ResultSet generatedKeys = statement.getGeneratedKeys())
+                    {
+                        if (generatedKeys.next())
+                        {
+                            int id = generatedKeys.getInt(1);
+                            user.setId(id);
+                        }
+                    }
                 }
-            }, key);
+            }
 
-        int update = this.jdbcTemplate.update(query,preparedStatement-> {
+        /*int update = this.jdbcTemplate.update(query,preparedStatement-> {
            preparedStatement.setString(1,user.getName());
            preparedStatement.setString(2,user.getEmail());
            preparedStatement.setString(3,user.getUsername());
@@ -55,32 +62,32 @@ public class UserDataManager {
         if (userId == -1){
             // Throw and Show the Error
         }
+        */
+            return user;
 
-        user.setId(userId);
-        return user;
-
-        }catch (EmptyResultDataAccessException e){
+        } catch (EmptyResultDataAccessException | SQLException e)
+        {
+            e.printStackTrace();
             return null;
         }
     }
 
-    public int getIdOfLastAddedRecord(User user){
+    public int getIdOfLastAddedRecord(User user)
+    {
         String query = "SELECT id from user where username = ?";
         int id = -1;
-        id = this.jdbcTemplate.queryForObject(query,new Object[] {user.getUsername()}, (resultSet, i) -> {
+        id = this.jdbcTemplate.queryForObject(query, new Object[]{user.getUsername()}, (resultSet, i) ->
+        {
             return resultSet.getInt("id");
         });
         return id;
     }
 
 
-
-
-
-    public List<Map<String,Object>> getAllUser()
+    public List<Map<String, Object>> getAllUser()
     {
         String query = "SELECT id, name, email, username, password, roleid FROM user";
-        List<Map<String,Object>> response =  jdbcTemplate.queryForList(query);
+        List<Map<String, Object>> response = jdbcTemplate.queryForList(query);
         return response;
     }
 
@@ -89,23 +96,24 @@ public class UserDataManager {
     {
         String query = "SELECT id, name, email, username, password, roleid FROM user WHERE id = ?";
 
-       try
-       {
-           return jdbcTemplate.queryForObject(query, new Object[]{id}, ((resultSet, i) ->
-           {
-               User user = new User();
-               user.setId(resultSet.getInt("id"));
-               user.setName(resultSet.getString("name"));
-               user.setEmail(resultSet.getString("email"));
-               user.setUsername(resultSet.getString("username"));
-               user.setPassword(resultSet.getString("password"));
-               user.setRoleid(resultSet.getInt("roleid"));
+        try
+        {
+            return jdbcTemplate.queryForObject(query, new Object[]{id}, ((resultSet, i) ->
+            {
+                User user = new User();
+                user.setId(resultSet.getInt("id"));
+                user.setName(resultSet.getString("name"));
+                user.setEmail(resultSet.getString("email"));
+                user.setUsername(resultSet.getString("username"));
+                user.setPassword(resultSet.getString("password"));
+                user.setRoleid(resultSet.getInt("roleid"));
 
-               return user;
-           }));
-       }catch (EmptyResultDataAccessException e){
-           return null;
-       }
+                return user;
+            }));
+        } catch (EmptyResultDataAccessException e)
+        {
+            return null;
+        }
 
     }
 
@@ -113,14 +121,15 @@ public class UserDataManager {
     {
         String query = "UPDATE user SET name = ? , email = ?, username = ?, password = ?, roleid = ? WHERE id = ?";
 
-        int update = this.jdbcTemplate.update(query, preparedStatement -> {
+        int update = this.jdbcTemplate.update(query, preparedStatement ->
+        {
 
             preparedStatement.setString(1, user.getName());
             preparedStatement.setString(2, user.getEmail());
             preparedStatement.setString(3, user.getUsername());
             preparedStatement.setString(4, user.getPassword());
             preparedStatement.setInt(5, user.getRoleid());
-            preparedStatement.setInt(6,user.getId());
+            preparedStatement.setInt(6, user.getId());
 
         });
 
@@ -134,8 +143,9 @@ public class UserDataManager {
 
         String query = "DELETE FROM user WHERE id = ?";
 
-        int update = this.jdbcTemplate.update(query,preparedStatement -> {
-           preparedStatement.setInt(1, id);
+        int update = this.jdbcTemplate.update(query, preparedStatement ->
+        {
+            preparedStatement.setInt(1, id);
         });
 
         return update == 1 ? user : null;
