a2344687a100ca277c84c0218ea60737ed662d74
Bhavik Savaliya <savaliyabhavic@gmail.com>
2018-09-26 15:47:54 +0530

    List of Products Page created in Vue

--- ui/product.js
+++ ui/product.js
@@ -1,4 +1,5 @@
 Vue.component('productform',{
+    name: 'productform',
     template: `
     <div class="productform">
     <div class="productFormView">
@@ -53,6 +54,131 @@ Vue.component('productform',{
         }
     });
 
+Vue.component('productList',{
+        name: 'productList',
+        template: `
+        <div class="productList">
+                <ul class="productListUl">
+                    <li v-for="item in productItems">
+                        <div class="productItem">
+                                <div class="productTileView">
+                                        <h4 class="productName">{{item.name}}</h4>
+                                </div>
+                            
+                               <div class="productDetilsView">
+                                    <p>{{item.descrption}}</p>
+                                    <p>Available Qty: {{item.qty}}</p>
+                                    <p>Price: {{item.price}} Rs.</p>
+                                    <button>ADD TO CART</button>
+                               </div>
+                               
+                        </div>
+                    </li>
+
+
+                </ul>
+        </div>
+        `,
+        data(){
+            return{
+                productItems: [
+                    {
+                        "id": 1,
+                        "name": "Dell Vostro",
+                        "descrption": "Laptos lalalall balla bla",
+                        "qty": 3,
+                        "price": 35000,
+                        "category": 1,
+                        "cid": 1,
+                        "cname": "Electronics"
+                    },
+                    {
+                        "id": 2,
+                        "name": "Shoool Beg",
+                        "descrption": "begs las las bla",
+                        "qty": 2,
+                        "price": 760,
+                        "category": 2,
+                        "cid": 2,
+                        "cname": "Men"
+                    },
+                    {
+                        "id": 3,
+                        "name": "Mi Bend HRX",
+                        "descrption": "Branded Fitness",
+                        "qty": 10,
+                        "price": 1200,
+                        "category": 2,
+                        "cid": 2,
+                        "cname": "Men"
+                    },
+                    {
+                        "id": 5,
+                        "name": "Fasttrack Watch",
+                        "descrption": "Branded watches",
+                        "qty": 4,
+                        "price": 2700,
+                        "category": 2,
+                        "cid": 2,
+                        "cname": "Men"
+                    },
+                    {
+                        "id": 6,
+                        "name": "WoodLand Shooes",
+                        "descrption": "Wond asnjsdbjbckb hsdabc",
+                        "qty": 2,
+                        "price": 3500,
+                        "category": 3,
+                        "cid": 3,
+                        "cname": "FootWear"
+                    },
+                    {
+                        "id": 8,
+                        "name": "Think Pad",
+                        "descrption": "laptop brands ",
+                        "qty": 4,
+                        "price": 30000,
+                        "category": 1,
+                        "cid": 1,
+                        "cname": "Electronics"
+                    },
+                    {
+                        "id": 9,
+                        "name": "Redmi Note 5 Pro",
+                        "descrption": "Phone lal lal lal",
+                        "qty": 10,
+                        "price": 15999,
+                        "category": 1,
+                        "cid": 1,
+                        "cname": "Electronics"
+                    },
+                    {
+                        "id": 10,
+                        "name": "Oprating System",
+                        "descrption": "Book ",
+                        "qty": 3,
+                        "price": 800,
+                        "category": 5,
+                        "cid": 5,
+                        "cname": "Study"
+                    },
+                    {
+                        "id": 11,
+                        "name": "Dell Laptops",
+                        "descrption": "bla ba blana",
+                        "qty": 2,
+                        "price": 20000,
+                        "category": 2,
+                        "cid": 2,
+                        "cname": "Men"
+                    }
+                ]
+            }},
+            methods:{
+                
+            }
+        });
+
 
 var app = new Vue({
     el:"#app",
@@ -61,5 +187,9 @@ var app = new Vue({
     },
     methods: {
         
+    },
+    components: {
+        'product-List': this.productList,
+        'product-form': this.productform
     }
 });
